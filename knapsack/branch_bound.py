db81da7464df5bae8de655e7afca5bcd5a923d54
Chris Elion <celion@gmail.com>
2015-03-06 23:40:09 -0800

    simple bounds, cache weight and value

--- knapsack/branch_bound.py
+++ knapsack/branch_bound.py
@@ -5,13 +5,27 @@ class State:
     def __init__(self):
         self.index = -1
         self.knapsack = set()
+        self.total_value = 0
+        self.total_weight = 0
 
-    def is_valid(self, capacity):
-        weight = sum(i.weight for i in self.knapsack)
-        return weight <= capacity
+    def is_valid(self, best_val, capacity, items):
+        if self.total_weight > capacity:
+            return False
+        best_case = self.get_best_case(items)
+        #print "Optimistic: %d  current best: %d" % (best_case, best_val)
+        if best_case <= best_val:
+            return False
+        return True
 
     def get_value(self):
-        return sum(i.value for i in self.knapsack)
+        return self.total_value
+
+    def get_best_case(self, items):
+        # Naive approach - best case is current value plus everything not tried yet
+        current_value = self.get_value()
+        for i in xrange(self.index, len(items)):
+            current_value += items[i].value
+        return current_value
 
 def get_taken(knapsack, items):
     taken = [0] * len(items)
@@ -64,17 +78,19 @@ def solve_branch_bound(capacity, items):
         l.index = current_state.index+1
         l.knapsack = current_state.knapsack.copy()
         l.knapsack.add(items[current_state.index])
+        l.total_value = current_state.total_value + items[current_state.index].value
+        l.total_weight = current_state.total_weight + items[current_state.index].weight
 
         r = State()
         r.index = current_state.index+1
         r.knapsack = current_state.knapsack.copy()
+        r.total_value = current_state.total_value
+        r.total_weight = current_state.total_weight
 
-        l_ok = l.is_valid(capacity)
-        r_ok = r.is_valid(capacity)
+        l_ok = l.is_valid(best_val, capacity, items)
+        r_ok = r.is_valid(best_val, capacity, items)
 
         if l_ok and r_ok:
-            # Push both
-            # TODO queue R and update current
             stack.append(r)
             current_state = l
         elif l_ok:
